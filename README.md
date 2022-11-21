a2025a7fceec73dc2355806d60675135f393d037
Jeremy Lei <jeremy.lei.2002@gmail.com>
2022-11-20 23:48:50 -0800

    still working on bug fixing

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Lab 8 - Starter
+Jeremy Lei
+
+1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+Within a Github action that runs whenever code is pushed. This is because it makes project development the easiest since it is done automatically for you and is thus always being done instead of relying on the coder to run tests.
+
+2) Would you use an end to end test to check if a function is returning the correct output? (yes/no) 
+
+No
+
+3) 
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -23,17 +23,20 @@ describe('Basic user flow for Website', () => {
     let data, plainValue;
     // Query select all of the <product-item> elements
     const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
-    // Grab the .data property of <product-items> to grab all of the json data stored inside
-    data = await prodItems[0].getProperty('data');
-    // Convert that property to JSON
-    plainValue = await data.jsonValue();
-    // Make sure the title, price, and image are populated in the JSON
-    if (plainValue.title.length == 0) { allArePopulated = false; }
-    if (plainValue.price.length == 0) { allArePopulated = false; }
-    if (plainValue.image.length == 0) { allArePopulated = false; }
-    // Expect allArePopulated to still be true
-    expect(allArePopulated).toBe(true);
+    for(let item in prodItems) {
+      let count = parseInt(item) + 1;
+      console.log(`Checking product item ${count}/${prodItems.length}`);
+      // Grab the .data property of <product-items> to grab all of the json data stored inside
+      data = await prodItems[item].getProperty('data');
+      // Convert that property to JSON
+      plainValue = await data.jsonValue();
+      // Make sure the title, price, and image are populated in the JSON
+      if (plainValue.title.length == 0) { allArePopulated = false; }
+      if (plainValue.price.length == 0) { allArePopulated = false; }
+      if (plainValue.image.length == 0) { allArePopulated = false; }
+      // Expect allArePopulated to still be true
+      expect(allArePopulated).toBe(true);
+    }
 
     // TODO - Step 1
     // Right now this function is only checking the first <product-item> it found, make it so that
@@ -47,9 +50,20 @@ describe('Basic user flow for Website', () => {
     console.log('Checking the "Add to Cart" button...');
     // TODO - Step 2
     // Query a <product-item> element using puppeteer ( checkout page.$() and page.$$() in the docs )
+    const prodItem = await page.$('product-item');
+
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
+    let shadow = await prodItem.getProperty('shadowRoot');
+    let button = await shadow.$('button');
+
     // Once you have the button, you can click it and check the innerText property of the button.
+    await button.click();
+    let innerText = button.getProperty('innerText');
+
     // Once you have the innerText property, use innerText.jsonValue() to get the text value of it
+     let value = await innerText.jsonValue()
+
+     expect(value).toBe("Remove from Cart");
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
